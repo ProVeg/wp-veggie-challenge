3aae40db93d92ca7bdbe5ba36ffed9655d7f2bef
Alexander Pols <apols@bol.com>
2018-04-21 14:23:30 +0200

    Add agree on emails user meta field to user edit profile page

--- includes/class-veggie-challenge.php
+++ includes/class-veggie-challenge.php
@@ -32,6 +32,7 @@ class Veggie_Challenge {
     static $USER_FIELD_CURRENT_DIET = 'veggie_challenge_current_diet';
     static $USER_FIELD_CHALLENGE_TYPE = 'veggie_challenge_challenge_type';
     static $USER_FIELD_START_DATE = 'veggie_challenge_start_date';
+    static $USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS = 'veggie_challenge_agree_veggie_challenge_emails';
 
     static $DIET_TYPES = Array(
         'vegan' => 'Vegan',
--- user-profile-fields.php
+++ user-profile-fields.php
@@ -26,7 +26,7 @@ function _vc_show_challenge_profile_fields( $user ) { ?>
         </tr>
 
         <tr>
-            <th><label for="<?php echo Veggie_Challenge::$USER_FIELD_CURRENT_DIET; ?>">Current diet</label></th>
+            <th><label for="<?php echo Veggie_Challenge::$USER_FIELD_CURRENT_DIET; ?>"><?php echo __("Current diet", 'veggie-challenge') ?></label></th>
 
             <?php
             $selected = esc_attr( get_the_author_meta( Veggie_Challenge::$USER_FIELD_CURRENT_DIET, $user->ID ) );
@@ -47,7 +47,7 @@ function _vc_show_challenge_profile_fields( $user ) { ?>
         </tr>
 
         <tr>
-            <th><label for="<?php echo Veggie_Challenge::$USER_FIELD_CHALLENGE_TYPE; ?>">Challenge type</label></th>
+            <th><label for="<?php echo Veggie_Challenge::$USER_FIELD_CHALLENGE_TYPE; ?>"><?php echo __("Challenge type", 'veggie-challenge') ?></label></th>
 
             <?php
             $selected = esc_attr( get_the_author_meta( Veggie_Challenge::$USER_FIELD_CHALLENGE_TYPE, $user->ID ) );
@@ -68,13 +68,28 @@ function _vc_show_challenge_profile_fields( $user ) { ?>
         </tr>
 
         <tr>
-            <th><label for="<?php echo Veggie_Challenge::$USER_FIELD_START_DATE; ?>">Start date</label></th>
+            <th><label for="<?php echo Veggie_Challenge::$USER_FIELD_START_DATE; ?>"><?php echo __("Start date", 'veggie-challenge') ?></label></th>
 
             <td>
                 <input type="date" name="<?php echo Veggie_Challenge::$USER_FIELD_START_DATE; ?>" id="<?php echo Veggie_Challenge::$USER_FIELD_START_DATE; ?>" value="<?php echo esc_attr( get_the_author_meta( Veggie_Challenge::$USER_FIELD_START_DATE, $user->ID ) ); ?>" class="date" />
                 Saved date: <?php echo esc_attr( get_the_author_meta( Veggie_Challenge::$USER_FIELD_START_DATE, $user->ID ) ); ?>
             </td>
 
+        </tr>
+
+        <tr>
+            <th><label><?php echo __("Agreed on VeggieChallenge emails", 'veggie-challenge') ?></label></th>
+
+            <?php
+            $participates = get_the_author_meta( Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS, $user->ID );
+            ?>
+            <td>
+                <input name="<?php echo Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS; ?>" id="<?php echo Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS; ?>_yes" type="radio" value="1" <?php echo $participates === "1" ? 'checked="checked"' : '' ?>>
+                <label for="<?php echo Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS; ?>_yes"><?php echo __("Yes") ?></label>
+                <input name="<?php echo Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS; ?>" id="<?php echo Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS; ?>_no" type="radio" value="0" <?php echo $participates === "1" ? '' : 'checked="checked"' ?>>
+                <label for="<?php echo Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS; ?>_no"><?php echo __("No") ?></label>
+                Saved key: <?php echo esc_attr( get_the_author_meta( Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS, $user->ID ) ); ?>
+            </td>
         </tr>
 
     </table>
@@ -88,8 +103,9 @@ function _vc_save_challenge_profile_fields( $user_id ) {
     if ( !current_user_can( 'edit_user', $user_id ) )
         return false;
 
+    update_user_meta( $user_id, 'participates_in_veggiechallenge', $_POST['participates_in_veggiechallenge'] );
     update_user_meta( $user_id, Veggie_Challenge::$USER_FIELD_CURRENT_DIET, $_POST[Veggie_Challenge::$USER_FIELD_CURRENT_DIET] );
     update_user_meta( $user_id, Veggie_Challenge::$USER_FIELD_CHALLENGE_TYPE, $_POST[Veggie_Challenge::$USER_FIELD_CHALLENGE_TYPE] );
     update_user_meta( $user_id, Veggie_Challenge::$USER_FIELD_START_DATE, $_POST[Veggie_Challenge::$USER_FIELD_START_DATE] );
-    update_user_meta( $user_id, 'participates_in_veggiechallenge', $_POST['participates_in_veggiechallenge'] );
+    update_user_meta( $user_id, Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS, $_POST[Veggie_Challenge::$USER_FIELD_AGREE_VEGGIE_CHALLENGE_EMAILS] );
 }
