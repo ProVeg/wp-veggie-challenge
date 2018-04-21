04c9000534b3b4c29f0d0a8649209b82078994d2
Ad van Wingerden <ad.van.wingerden@proveg.com>
2018-04-21 13:47:00 +0200

    added field for participation in veggiechallenge

--- hooks.php
+++ hooks.php
@@ -9,29 +9,50 @@
   * @param array  $form     Contains information about the form
  */
 function set_post_content( $entry, $form ) {
+    $form_error = new WP_Error();
+
     $veggie_challenge_form_id = intval(get_option('veggie_challenge_gravity_forms_form_id'));
     $veggie_challenge_role_id = get_role('veggiechallenge');
+    $email_address_field_id = 2;
+    $challenge_field_id = 2;
+    $agree_field_id = 6;
 
     // check if this is the right form
     if ($form['id'] !== $veggie_challenge_form_id){
         return;
     }
     
-    $email_address_field_id = 2;
     $email_address = $entry[$email_address_field_id];
+    $challenge = $entry[$challenge_field_id];
 
-    // check if the user exists
-    $user_id = username_exists( $email_address );
-
-    // if the user does not exist, create it
-    if (!$user_id && email_exists( $email_address ) == false ) {
-        $random_password = wp_generate_password( $length=16, $include_standard_special_chars=false );
-        $user_id = wp_create_user( $email_address, $random_password, $email_address );
-        wp_update_user( array( 'ID' => $user_id, 'role' => 'veggiechallenge' ) );
-        //update_user_meta( $user_id, $meta_key, $meta_value, $prev_value );
+    $agree = $entry["$agree_field_id.1"];
+    if (!$agree || $agree === __("No") || $agree === __("no")) {
+        $form_error->add( 'agree', __("Agree should not be empty and not equal to 'no'"));
     }
 
+    if ( is_wp_error( $form_error ) ) {
+        foreach ( $form_error->get_error_messages() as $error ) {
+            echo '<div>';
+            echo '<strong>' . __("ERROR") . ':</strong>:';
+            echo $error . '<br/>';
+            echo '</div>';
+        }
+    }
 
+    if ($form_error->get_error_messages() === 0) {
+
+        // check if the user exists
+        $user_id = username_exists( $email_address );
+    
+        // if the user does not exist, create it
+        if (!$user_id && email_exists( $email_address ) == false ) {
+            $random_password = wp_generate_password( $length=16, $include_standard_special_chars=false );
+            $user_id = wp_create_user( $email_address, $random_password, $email_address );
+            wp_update_user( array( 'ID' => $user_id, 'role' => 'veggiechallenge' ) );
+        }
+    
+        update_user_meta( $user_id, 'participates_in_veggiechallenge', '1');
+    }
 }
 add_action( 'gform_after_submission', 'set_post_content', 10, 2 );
 
--- user-profile-fields.php
+++ user-profile-fields.php
@@ -6,9 +6,24 @@ add_action( 'edit_user_profile', '_vc_show_challenge_profile_fields' );
 
 function _vc_show_challenge_profile_fields( $user ) { ?>
 
-    <h3>Veggie Challenge Fields</h3>
+    <h3><?php echo __("VeggieChallenge Fields") ?></h3>
 
     <table class="form-table">
+
+        <tr>
+            <th><label><?php echo __("Participates") ?></label></th>
+
+            <?php
+            $participates = get_the_author_meta( 'participates_in_veggiechallenge', $user->ID );
+            ?>
+            <td>
+                <input name="participates_in_veggiechallenge" id="participates_in_veggiechallenge_yes" type="radio" value="1" <?php echo $participates === "1" ? 'checked="checked"' : '' ?>>
+                <label for="participates_in_veggiechallenge_yes"><?php echo __("Yes") ?></label>
+                <input name="participates_in_veggiechallenge" id="participates_in_veggiechallenge_no" type="radio" value="0" <?php echo $participates === "1" ? '' : 'checked="checked"' ?>>
+                <label for="participates_in_veggiechallenge_no"><?php echo __("No") ?></label>
+                Saved key: <?php echo esc_attr( get_the_author_meta( 'participates_in_veggiechallenge', $user->ID ) ); ?>
+            </td>
+        </tr>
 
         <tr>
             <th><label for="current_diet">Current diet</label></th>
@@ -70,4 +85,5 @@ function _vc_save_challenge_profile_fields( $user_id ) {
     update_user_meta( $user_id, 'current_diet', $_POST['current_diet'] );
     update_user_meta( $user_id, 'challenge_type', $_POST['challenge_type'] );
     update_user_meta( $user_id, 'start_date', $_POST['start_date'] );
+    update_user_meta( $user_id, 'participates_in_veggiechallenge', $_POST['participates_in_veggiechallenge'] );
 }
